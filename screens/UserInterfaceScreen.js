e717efe40b8392a02032568c3010947980f9e061
beastly19802 <69489254+beastly19802@users.noreply.github.com>
2021-02-26 20:37:44 -0500

    Add files via upload

---
+++ screens/UserInterfaceScreen.js
@@ -0,0 +1,15 @@
+import React,{Components} from 'react'
+import { render } from 'react-dom'
+import {Text, View, TouchableOpacity} from 'react-native'
+
+/*export default class UserInterfaceScreen extends Components{
+render(){
+    return(
+        <View>
+            <Text>
+                it works
+            </Text>
+        </View>
+    )
+}
+}*/
